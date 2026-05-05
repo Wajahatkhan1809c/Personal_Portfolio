a9705ef4ddf37a53e2106c21346471e32d10bd13
wajahat1809 <wajahat@aptechgdn.net>
2026-05-05 14:46:06 +0500

    portfolio

---
+++ gsap.js
@@ -0,0 +1,18 @@
+gsap.from('.whatsapp-icon',{
+    duration: 1.5,
+    x: 100,
+    opacity: 0,
+    ease: 'power2.out',
+})
+gsap.from('.linkedin-icon',{
+    duration: 1.5,
+    x: -100,
+    opacity: 0,
+    ease: 'power2.out',
+})
+gsap.from('.navbardivpositionwala',{
+    duration: 1.5,
+    y: -100,
+    opacity: 0,
+    ease: 'power2.out',
+})
